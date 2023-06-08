29eb6a44abea3e0bb0cd2ff9dafafb0a47b12144
Andrius <andriuscanis@gmail.com>
2023-06-08 09:15:04 +0300

    quantity in shop

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 console.clear();
 
 import { Shop } from "./Js/Shop.js";
+import { Item } from "./Js/Shop.js";
 
-const candyShop = new Shop("CandyShop", "eur", );
-
+const candyShop = new Shop("CandyShop", "eur", 0 );
 candyShop.greeting()
-//console.log(candyShop)
-//console.log(candyShop.currency);
 
+//console.log(candyShop);
+//console.log(candyShop.currency);
+//console.log(candyShop.inventor);
+
+console.log('----------------------')
+
+const goods = new Item(1, 'candyPack');
+
+//console.log(goods);
+//console.log(goods.goodsId);
+//console.log(goods.goodsName);
+//console.log(goods.goodsBuyPrice);
+//console.log(goods.goodsSalePrice);
+//console.log(goods.goodsQuantity);
+//console.log(goods.goodsSoldQuantity);
+console.log(goods.restock(45));
+console.log(goods);
+console.log(goods.goodsInShop())
--- js/Shop.js
+++ js/Shop.js
@@ -1,12 +1,38 @@
 
 export class Shop {
-  constructor(shopName, currency) {
-    this.shopName = shopName;
+  constructor(candyShop, currency, inventor) {
+    this.shopName = candyShop;
     this.currency = currency;
+    this.inventor = inventor;
   }
-
 
   greeting() {
-    console.log( `Sveiki atvykę į ${this.shopName} !`);
+    console.log( `Sveiki atvykę į ${this.shopName}!`);
   }
 }
+
+
+export class Item {
+  constructor(goodsId, goodsName) {
+    this.goodsId = goodsId
+    this.goodsName = goodsName
+    this.goodsBuyPrice = 2
+    this.goodsSalePrice = 3
+    this.goodsQuantity = 0
+    this.goodsSoldQuantity = 0
+  }
+
+  restock(refill) {
+    this.goodsQuantity += refill
+    return `Likutis papildytas ${refill}vnt`
+  }
+
+  goodsInShop() {
+    if (this.goodsQuantity === 0) {
+      return "Sios prekes nebeturime"
+    } else {
+      return `${this.goodsQuantity}vnt `
+    }
+  }
+}
+
